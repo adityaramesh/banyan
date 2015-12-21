4aa3d39dc8c58db815963ed51e4c6a729c27764c
Aditya Ramesh <_@adityaramesh.com>
2015-12-20 23:41:49 -0500

    Now checking status codes; added test for job creation.

--- test/test_server.py
+++ test/test_server.py
@@ -16,7 +16,12 @@ def drop_database():
 def post(resource, data):
 	entry_point = 'http://127.0.0.1:5000'
 	url = urljoin(entry_point, resource) if resource else entry_point
-	return requests.post(url, data=data)
+	headers = {'Content-Type': 'application/json'}
+
+	# If we try to format the request without converting the JSON to a
+	# string first, the requests API will pass the parameters as part of
+	# the URL. As a result, nested JSON will not get encoded correctly.
+	return requests.post(url, headers=headers, data=json.dumps(data))
 
 def test_group_creation():
 	groups = [
@@ -25,24 +30,53 @@ def test_group_creation():
 		{'name': 'group_3', 'foo': 'bar'}
 	]
 
+	codes = [
+		requests.codes.created,
+		requests.codes.unprocessable_entity,
+		requests.codes.unprocessable_entity
+	]
+
 	print("Testing group creation.")
-	for doc in groups:
-		print(post('groups', doc).json())
+	for doc, code in zip(groups, codes):
+		assert post('groups', doc).status_code == code
 
 def test_job_creation():
-	#'gpus': [{'memory': '8 Gb', 'min_compute_capability': '5.0'}]
-	basic_job = {
-		'command': 'ls',
-		'requested_resources': {'memory': '64 Gb'}
-	}
-
 	jobs = [
-		basic_job
+		{
+			'command': 'ls',
+			'requested_resources': {
+				'memory': '64 GiB'
+			}
+		},
+		{
+			'command': 'ls',
+			'requested_resources': {
+				'memory': '64 GiB',
+				'cores': 8
+			}
+		},
+		{
+			'command': 'ls',
+			'requested_resources': {
+				'memory': '64 GiB',
+				'cores': 8,
+				'gpus': [{
+					'memory': '8 GiB',
+					'min_compute_capability': '5.0'
+				}]
+			}
+		}
+	]
+
+	codes = [
+		requests.codes.created,
+		requests.codes.created,
+		requests.codes.created
 	]
 
 	print("Testing job creation.")
-	for doc in jobs:
-		print(post('jobs', doc).raw)
+	for doc, code in zip(jobs, codes):
+		assert post('jobs', doc).status_code == code
 
 def test_schema():
 	test_group_creation()
