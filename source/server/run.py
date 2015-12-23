16f03da41cc5a7aac2a634af9b92285db697f0ed
Aditya Ramesh <_@adityaramesh.com>
2015-12-22 23:54:45 -0500

    Now uses IP of machine.

--- source/server/run.py
+++ source/server/run.py
@@ -5,8 +5,12 @@ Starts a server that maintains a shared job queue.
 """
 
 import os
+import socket
 from eve import Eve
+
+def get_ip():
+	return socket.gethostbyname(socket.gethostname())
 
 if __name__ == '__main__':
 	app = Eve()
-	app.run(host='127.0.0.1', port=5000)
+	app.run(host=get_ip(), port=5000)
