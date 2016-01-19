5b453a732a46dcea8da826cad64778c9e9dbfc12
Aditya Ramesh <_@adityaramesh.com>
2016-01-19 15:28:13 -0500

    Full validation for add_continuations now working.

--- banyan/server/virtual_resources.py
+++ banyan/server/virtual_resources.py
@@ -5,22 +5,147 @@ Defines the virtual subresources used to add items to and remove items from
 list-based fields.
 """
 
-# TODO reorganize later
-from datetime import datetime
+from copy import copy
 from bson import ObjectId
-from flask import Blueprint, abort, request, current_app as app
-from eve.utils import config, parse_request
-from eve.render import send_response
-from eve.methods import patch
+from datetime import datetime
 
-from eve.method.common import get_document, parse, payload as payload_, store_media_files, \
-	resolve_embedded_fields
-from eve.versioning import resolve_document_version, late_versioning_catch
-import pdb
+from werkzeug import exceptions
+from flask import Blueprint, current_app as app
+
+from eve.defaults import build_defaults
+from eve.utils import *
+from eve.validation import ValidationError
+from eve.methods.common import *
+from eve.versioning import *
+
+from validation import Validator
 
 blueprint = Blueprint('virtual_resources', __name__)
 
 """
+When the user POSTs to a virtual subresource, it is understood that all of the
+requested updates are to made in one atomic transaction. The `Mongo` data layer
+provided by Eve doesn't implement this functionality. Instead, we use the
+following steps:
+  1. Validate the JSON array of updates.
+  2. Use the JSON array to construct one large batch update operation.
+  3. Execute the update operation.
+
+The problem here is that MongoDB imposes a limit of 16 Mb on any BSON document.
+So the update will be invalid if it is too large. To ensure that this doesn't
+happen, we restrict the sizes of the following:
+  1. The number of updates in the JSON array.
+  2. The size of the 'targets' and 'updates' lists for each update.
+
+We assume that the maximum size of any element in the 'target' or 'updates'
+list is 128 bytes. With the constants as defined below, the maximum update size
+is precisely 16 Mb. Atomic updates larger than this cannot be made.
+"""
+max_update_list_length = 128
+max_item_list_length = 1024
+
+virtual_resource_keys = {'targets', 'values'}
+
+"""
+Note: we could disallow empty lists for targets using `'empty': False`, since
+the update would effectively be a no-op. But technically, returning success in
+this case is the right thing to do.
+"""
+target_schema = {
+	'targets': {
+		'type': 'list',
+		'maxlength': max_item_list_length,
+		'allows_duplicates': False,
+		'schema': {
+			'type': 'objectid',
+			'data_relation': {
+				'resource': 'tasks',
+				# The key 'field' defaults to _id, so normally, we would not need to
+				# provide it explcitly. These default values for schema are set in
+				# flaskapp.py. However, it's easier to provide the default value
+				# manually than to apply the existing code to do it for us.
+				'field': '_id'
+			}
+		}
+	}
+}
+
+
+"""
+Note: we could disallow empty lists for updates using `'empty': False`, since
+the update would effectively be a no-op. But technically, returning success in
+this case is the right thing to do.
+"""
+continuation_update_schema = copy(target_schema)
+continuation_update_schema['values'] = {
+	'type': 'list',
+	'maxlength': max_item_list_length,
+	'allows_duplicates': False,
+	'schema': {
+		'type': 'objectid',
+		'data_relation': {
+			'resource': 'tasks',
+			'field': '_id'
+		}
+	}
+}
+
+def validate_updates(updates, schema, extra_valid_func=None):
+	if not isinstance(updates, list):
+		return updates, {'input': "Updates string must be a JSON array."}
+
+	if len(updates) > max_update_list_length:
+		return updates, {'input', "Updates array can have at most {} elements.". \
+			format(max_update_list_length)}
+
+	issues = {}
+
+	for i, update in enumerate(updates):
+		if not isinstance(update, dict):
+			issues['update {}'.format(i)] = "Array element is not a dict."
+			continue
+
+		cur_issues = []
+		for key in virtual_resource_keys:
+			if key not in update:
+				cur_issues.append("Missing field '{}'.".format(key))
+
+		invalid_keys = update.keys() - virtual_resource_keys
+		if len(invalid_keys) != 0:
+			cur_issues.append("Invalid keys '{}'.".format(invalid_keys))
+
+		if len(cur_issues) > 0:
+			issues['update {}'.format(i)] = str(cur_issues)
+
+	if len(issues) > 0:
+		return updates, issues
+
+	validator = Validator(schema=schema, resource='tasks')
+	revised_updates = []
+
+	for i, update in enumerate(updates):
+		# This deserializes some values that require special handlers,
+		# such as ObjectIds.
+		# TODO: fill in default fields for the schema.
+		# TODO: fill in default values for list elements, if specified.
+		revised_update = serialize(update, schema=schema)
+		revised_updates.append(revised_update)
+
+		def log_issue(msg):
+			issues['update {}: validator errors'.format(i)] = msg
+
+		if not validator.validate(revised_update):
+			log_issue(str(validator.errors))
+			continue
+
+		if extra_valid_func:
+			extra_valid_func(revised_update, log_issue)
+
+	return revised_updates, issues
+
+"""
+TODO revise description:
+
 Given the name of an array-valued field, this function appends given elements
 to the corresponding array of each task in a given set.
 
@@ -41,9 +166,6 @@ Parameters:
 - `values`: Elements to append to the array for each task.
 - `valid_func`: Function used to validate the 
 
-*** KEEP SOME OF THE IMPORTANT NOTES IN THE DOC (e.g. about datalayer) BUT
-REMOVE THE REST LATER ***
-
 - Notes: how to make `continuations` be treated like a set.
   - Ideally, we would just always use Mongo's $addToSet operator. But Eve's
     `Mongo` `DataLayer` is only capable of performing updates using
@@ -56,23 +178,13 @@ REMOVE THE REST LATER ***
     this case, we perform the insertion by ourselves, so we can use `$addToSet`
     without removing duplicates beforehand. Subclass `Mongo` for this.
 
-- Use the schema of the values array (e.g. so that we can create a
-  new Validator and implicitly use `_validate_data_relation`
-  `_validate_type_objectid` to check correctness). We can just obtain this
-  schema directly from the one defined in settings.py; the programmer doesn't
-  need to provide it.
-- http://stackoverflow.com/questions/3144737/multiple-simultaneous-updates-with-mongodb-pymongo
-
 -------------------------------------------------------------------------------
 
 - TODO: also allow the user to use virtual subresources in regular POST
   requests, e.g. so that other fields of a task can be modified using the same
-  request that adds/removes continuations. To do this, use one of the methods
-  below.
+  request that adds/removes continuations. To do this, use the method below.
 
-- Don't use the method below unless method 1 is infeasible.
-
-- Method 1. This method involves subclassing the eve.io.mongo.Mongo DataLayer.
+- This method involves subclassing the eve.io.mongo.Mongo DataLayer.
   - Remember to set the DataLayer of the Eve app to the subclassed version of
     the DataLayer when starting Eve.
   - Override the `update` function of `Mongo`.
@@ -84,88 +196,55 @@ REMOVE THE REST LATER ***
   - Use `$pull` to remove elements. We need to do special formatting for this
     query, so that the argument of the operator is a set of dictionaries of the
     form `{'_id': <id>}`.
-
-- Method 2. WARNING: the procedure below is NOT a robust, production-ready
-  solution. Since all of the updates will no longer be performed using the same
-  transaction sent to the database, the update will not be atomic if either the
-  regular updates or the updates to the virtual subresources fail.
-  - Use a pre-event hook to (1) convert the body of the request payload into
-    JSON, and (2) filter out any virtual subresources from it. If possible, try
-    to reuse this JSON when the request is forwarded to the internals of Eve.
-  - In the pre-event hook, we need to validate all of the virtual subresource
-    fields. Otherwise, we can't guarantee that the entire PATCH operation will
-    be atomic. If validation fails, return in the same way that
-    `patch_internal` does.
-  - Save the PATCH fields that we have filtered out somehow (e.g. using Flask's
-    request object, if it's valid to do this).
-  - Post-event hooks are called in `render.py`; `send_response` is wrapped by a
-    decorator that calls the appropriate post-event hooks before returning the
-    response. This allows us to modify the response, if necessary.
-  - In the post-event hook, do the following. If the current response already
-    indicates failure, then do nothing. Otherwise, for each virtual subresource
-    field that was saved, perform the update to it. If it fails, alter the
-    current response so that it has the correct status code and message. Then
-    return. Otherwise, don't alter the response.
-"""
-
-# TODO offload the validation step to a separate function, so that we can reuse
-# this in the pre-event hook to allow virtual subresources to be used in general
-# PATCH requests. If `validate=False`, don't call this function (as it should
-# be been done already by the pre-event hook).
-def append_to_lists(field, updates, validate=True):
-	target_schema = {}
-
-
-	resource = 'tasks'
-	original = get_document(resource, concurrency_check=False, **lookup)
-
-	if not original:
-		abort(404)
-
-	resource_def = app.config['DOMAIN'][resource]
-	object_id = original[resource_def['id_field']]
-	
-	if config.BANDWIDTH_SAVER:
-		embedded_fields = []
-	else:
-		req = parse_request(resource)
-		embedded_fields = resolve_embedded_fields(resource, req)
-
-	try:
-		# Converts the payload (a string) into JSON.
-		updates = parse(payload, resource)
-
-		if validation_func(updates, object_id, original):
-			# Apply coerced values
-
-			# Sneak in a shadow copy if it wasn't already there
-			late_versioning_catch(original, resource)
-
-			# Stores any media files (defined in the resource
-			# schema) in advance, before processing the document.
-			store_media_files(updates, resource, original)
-
-			# If document versioning is enabled, then the function
-			# below updates the version number of the document.
-			resolve_document_version(updates, resource, 'PATCH', original)
-
-			updates[config.LAST_UPDATED] = datetime.utcnow().replace(microsecond=0)
-			
-"""
-The payload must be a JSON object whose keys are either `ObjectId`s or arrays
-of `ObjectId`s of the parent tasks. The values must be arrays of `ObjectId`s of
-the continuation tasks. This design allows for:
-
-  1. Efficient addition of the same continuations to many parents.
-  2. Efficient bulk insertion of continuations to many parents.
 """
 @blueprint.route('/tasks/add_continuations', methods=['POST'])
 def add_continuations():
-	resource = 'tasks'
+	# JSON representation of request payload.
+	updates = payload()
+
+	issues = {}
+	response = {}
 	
-	# Payload: {'continuations': [...]}.
-	# Ensure that payload has only that key.
-	# Ensure that each id is a valid objectid using the same code that eve does.
-	# Ensure no duplicates.
-	#response = patch(resource, **{config.ID_FIELD: ObjectId(task_id)})
-	return send_response(resource, response)
+	try:
+		def check_self_loops(updates, log_issue):
+			t = updates['targets']
+			u = updates['values']
+
+			if len(set(u) - set(t)) != len(u):
+				log_issue("Field 'updates' mentions values in 'targets'.")
+
+		updates, errors = validate_updates(updates, continuation_update_schema,
+			check_self_loops)
+
+		if not errors:
+			# TODO
+			pass
+		else:
+			issues = errors
+	except ValidationError as e:
+		issues['validator exception'] = str(e)
+	except exceptions.HTTPException as e:
+		raise e
+	except Exception as e:
+		app.logger.exception(e)
+		abort(400, description=debug_error_message("An exception occurred: {}".format(e)))
+
+	print(issues)
+		
+	#if len(issues) > 0:
+	#	response[config.ISSUES] = issues
+	#	response[config.STATUS] = config.STATUS_ERR
+	#	status = config.VALIDATION_ERROR_STATUS
+	#else:
+	#	response[config.STATUS] = config.STATUS_OK
+	#	status = 200
+
+	#response = marshal_write_response(response, 'tasks')
+
+	# We don't support concurrency control, and by returning `None` for
+	# 'last modified', we simply return the current time.
+	# todo incorporate stuff below into response using send_response
+	#etag = None
+	#last_modified = None
+	#, last_modified, etag, None #status= None
+	return "poop" 
