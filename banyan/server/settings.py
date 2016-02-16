2699e35c8fad1311d9bc6e7d8cdfd265d8ab3a9b
Aditya Ramesh <_@adityaramesh.com>
2016-02-15 20:25:35 -0500

    Minor formatting.

--- banyan/server/settings.py
+++ banyan/server/settings.py
@@ -18,9 +18,9 @@ DEBUG = True
 We assume that MongoDB is set up with no access control, so we don't need to
 use any credentials.
 """
-MONGO_HOST         = os.environ.get('MONGO_HOST', 'localhost')
-MONGO_PORT         = os.environ.get('MONGO_PORT', 27017)
-MONGO_DBNAME       = os.environ.get('MONGO_DBNAME', 'banyan')
+MONGO_HOST   = os.environ.get('MONGO_HOST', 'localhost')
+MONGO_PORT   = os.environ.get('MONGO_PORT', 27017)
+MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'banyan')
 
 # Disable etag concurrency control.
 IF_MATCH = False
